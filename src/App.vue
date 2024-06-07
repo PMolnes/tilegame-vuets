5ccf547a6e1cb68726a303cf75e9aa5367cc1971
PMolnes <pettermolnes@icloud.com>
2024-06-07 15:39:06 +0200

    (styling): semibold title

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+	import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header class="p-4 flex flex-col">
-    <nav class="flex justify-between">
-      <RouterLink to="/">GAME</RouterLink>
-      <RouterLink to="/highscores">HIGHSCORES</RouterLink>
-    </nav>
-    <h1 class="flex self-center h-full items-center text-3xl">SOLWR SOLWR</h1>
-  </header>
-  <body class="bg-black p-4">
-      <RouterView v-slot="{ Component }">
-        <KeepAlive>
-          <component :is="Component" />
-        </KeepAlive>
-      </RouterView>
-  </body>
+	<header class="p-4 flex flex-col">
+		<nav class="flex justify-between">
+			<RouterLink to="/">GAME</RouterLink>
+			<RouterLink to="/highscores">HIGHSCORES</RouterLink>
+		</nav>
+		<h1 class="flex self-center h-full items-center text-3xl font-semibold">SOLWR SOLWR</h1>
+	</header>
+	<body class="bg-black p-4">
+		<RouterView v-slot="{ Component }">
+			<KeepAlive>
+				<component :is="Component" />
+			</KeepAlive>
+		</RouterView>
+	</body>
 </template>
 
 <style>
-#app {
-  width: 100vw;
-  height: 100vh;
-  display: grid;
-  grid-template-rows: 10rem 1fr;
-  font-family: 'Fira Code', monospace;
-  background-color: black;
-  color: #EBFF00;
-}
+	#app {
+		width: 100vw;
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 10rem 1fr;
+		font-family: "Fira Code", monospace;
+		background-color: black;
+		color: #ebff00;
+	}
 </style>
